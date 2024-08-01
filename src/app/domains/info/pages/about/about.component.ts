fcc2674b65fd4f08d277ab869826dc0088d83f70
Ramiro Guzmán C. <ramiroguzmanc@gmail.com>
2024-08-01 14:55:31 -0500

    Agregando play pause en about

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,11 +1,13 @@
 import {Component, signal} from '@angular/core';
 import {CounterComponent} from "../../../shared/components/counter/counter.component";
+import {WaveAudioComponent} from "../../components/wave-audio/wave-audio.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
 	imports: [
-		CounterComponent
+		CounterComponent,
+		WaveAudioComponent
 	],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
