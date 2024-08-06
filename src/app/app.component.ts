c7c458f1f1dc91aea86b5392f8a517139130bd45
Ramiro Guzmán C. <ramiroguzmanc@gmail.com>
2024-08-06 16:25:22 -0500

    Vistas anidadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "./domains/shared/components/header/header.component";
+import {HeaderComponent} from "@shared/components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent],
-  templateUrl: 'app.component.html'
+  template: '<router-outlet/>'
 })
 export class AppComponent {
   title = 'store';
