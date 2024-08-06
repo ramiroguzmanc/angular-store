02c9c415fccb6a889eea2b5496c071f4501b9506
Ramiro Guzmán C. <ramiroguzmanc@gmail.com>
2024-08-06 16:48:26 -0500

    Routerlink active

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,14 +1,15 @@
 import {Component, inject} from '@angular/core';
 import {CartService} from "@shared/services/cart.service";
 import {CartComponent} from "@shared/components/cart/cart.component";
-import {RouterLink} from "@angular/router";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
 	imports: [
 		CartComponent,
-		RouterLink
+		RouterLink,
+		RouterLinkActive
 	],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
